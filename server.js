47650c42c018f33bd811cb655c164f533d31abe2
sujal092005 <sujaldudhe0@gmail.com>
2025-09-11 09:39:45 +0530

    Update server.js for better database connection handling

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { nanoid } = require('nanoid');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
